f361c7d1b4ba15c60ab39440d47724eae3717308
James Spolsdoff <jspolsdoff@gmail.com>
2015-01-31 10:52:17 -0800

    Finished everything but fstream menu functions.
    Finished all menu functions to add, remove, and display grocery list.
    Now I need to debug on flip.

--- cs162/wk4/assignment2/main.cpp
+++ cs162/wk4/assignment2/main.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************************************
 *Author:                 James Spolsdoff
 *Date Created:           1/28/14
-*Last Modification Date: 1/28/14
+*Last Modification Date: 1/30/14
 *Filename:               main.cpp
 *
 *Overview
@@ -13,23 +13,24 @@
 #include <fstream>
 #include <vector>
 #include <stdlib.h>
+#include <iomanip>
 
 // define the 'item' class
 class Item
 {
 	private:
 	std::string name;		// name of item
-	int unit;				// unit of measure
+	std::string unit;		// unit of measure
 	int quantity_needed;	// number of 'item' to buy
 	double price;			// unit price
 	
 	public:
 	Item();
-	Item(std::string, int, int, double);	// default constructor
-	std::string getName();								// display name of item
-	int getUnit();								// display the unit of item
-	int getQuantity();							// display the number to buy
-	double getPrice();								// display the unit price
+	Item(std::string, string, int, double);	// default constructor
+	std::string getName();					// display name of item
+	std::string getUnit();					// display the unit of item
+	int getQuantity();						// display the number to buy
+	double getPrice();						// display the unit price
 };
 
 // define the 'list' class
@@ -41,39 +42,38 @@ class List
 	std::ofstream outputList;				// output file stream
 	
 	public:
-	List();									// constructor
-	void addItem();								// adds an item to list vector
-	void removeItem();							// removes an item from list vector
-	void displayList();							// displays the list
-	void readList();								// read a saved list
-	void saveList();								// save a list to file
+	void addItem();							// adds an item to list vector
+	void removeItem(int);					// removes an item from list vector
+	void displayList();						// displays the list
+	void readList();						// read a saved list
+	void saveList();						// save a list to file
 };
 
 int main()
 {
-	Item item;								// item name variable
+	Item new_item;							// item name variable
 	List shoppingList;						// create list object
 	std::string name;						// item name
-	int unit;								// unit of measure variable
+	std::string unit;						// unit of measure variable
 	int quantity;							// quantity to buy variable
 	double price;							// unit price variable
 	int menu_choice;						// menu choice variable
-	int item_delete;						// variable to choose what item to delete
+	int remove_item;						// variable to choose what item to delete
 	
 	// contain menu in do/while loop
 	do {
 		// display menu
 		std::cout << "\nWELCOME TO THE GROCERY LIST PROGRAM\n" << std::endl;
 		std::cout << "Please choose from the following options" << std::endl;
-		std::cout << "1) Add Item to Grocery List" << std::endl;
-		std::cout << "2) Remove Item from Grocery List" << std::endl;
-		std::cout << "3) Display Grocery List" << std::endl;
-		std::cout << "4) Read List From File" << std::endl;
-		std::cout << "5) Save List To File" << std::endl;
-		std::cout << "6) Quit" << std::endl;
+		std::cout << "1) ADD ITEM TO GROCERY LIST" << std::endl;
+		std::cout << "2) REMOVE ITEM FROM GROCERY LIST" << std::endl;
+		std::cout << "3) DISPLAY GROCERY LIST" << std::endl;
+		std::cout << "4) READ LIST FROM FILE" << std::endl;
+		std::cout << "5) SAVE LIST TO FILE" << std::endl;
+		std::cout << "6) QUIT" << std::endl;
 		
 		// ask user for menu input
-		std::cout << "Please enter your selection: ";
+		std::cout << "\nPlease enter your selection: ";
 		
 		// store in variable
 		std::cin >> menu_choice;
@@ -86,7 +86,7 @@ int main()
 		}  
 		
 		// add item option
-		if (menu_choice == 1)
+		case 1:
 		{
 			std::cout << "\nPlease enter the follow information about the item\n" << std::endl;
 			
@@ -96,7 +96,7 @@ int main()
 
 			// ask and store unit of measure
 			std::cout << "Unit of Measure: ";
-			std::cin >> unit;
+			std::getline(std::cin, unit);
 			
 			// ask and store quantity to buy
 			std::cout << "Quantity: ";
@@ -107,37 +107,55 @@ int main()
 			std::cin >> price;
 			
 			// pass variables to function and add item to list
-			Item item(name, unit, quantity, price);
+			Item new_item(name, unit, quantity, price);
+			
+			// pass new_item object to grocery list object
+			shoppingList.addItem(new_item);
+			
+			// display confirmation message
+			std::cout << "Your item has been successfully added to the list!" << std::endl;
+		
+			break;	
 		}
 		
 		// remove item
-		else if (menu_choice == 2)
+		case 2:
 		{
 			// ask user for what item they want remove
+			std::cout << "What item number would you like to remove from your list: ";
+			std::cin >> remove_item;
+			remove_item--;
+			
 			// pass variable to function
+			shoppingList.removeItem(remove_item);
+			break;	
 		}
 		
 		// display list
-		else if (menu_choice == 3)
+		case 3:
 		{
 			// call function to display list
+			shoppingList.displayList();
+			break;	
 		}
 		
 		// read saved list
-		else if (menu_choice == 4)
+		case 4:
 		{
-		
+			break;	
 		}
 		
 		// save list
-		else if (menu_choice == 5)
+		case 5:
 		{
-		
+			break;	
 		}
 		// quit program
-		else
+		case 6:
+		{
 			exit(EXIT_SUCCESS);
-			
+		}
+		
 	} while(menu_choice != 6);	
 		
 	return 0;
@@ -160,7 +178,7 @@ Item::Item()
 *						Item
 *Default constructor for the Item class.
 ***************************************************************************************************/ 
-Item::Item(std::string a, int b, int c, double d)
+Item::Item(std::string a, std::string b, int c, double d)
 {
 	name = a;
 	unit = b;
@@ -168,12 +186,99 @@ Item::Item(std::string a, int b, int c, double d)
 	price = d; 
 }
 
+/**************************************************************************************************
+ *						getName
+ *
+ **************************************************************************************************/
+ std::string Item::getName()
+ {
+	return name;
+ }
+ 
+ /**************************************************************************************************
+ *						getUnit
+ *
+ **************************************************************************************************/
+ std::string Item::getUnit()
+ {
+	return unit;
+ }
+ 
+ /**************************************************************************************************
+ *						getQuantity
+ *
+ **************************************************************************************************/
+ int Item::getQuantity()
+ {
+	return quantity;
+ }
+ 
+ /**************************************************************************************************
+ *						getPrice
+ *
+ **************************************************************************************************/
+double Item::getPrice()
+{
+	return price;
+}
+
 // below are the 'list' member functions
 /**************************************************************************************************
 *						List
 *Default constructor for the List object.
 ***************************************************************************************************/ 
-List::List()
-{
+// List::List()
+// {
+//
+// }
 
+/**************************************************************************************************
+*						addItem
+*
+**************************************************************************************************/
+void List::addItem(Item new_item)
+{
+	item_list.push_back(new_item);
+} 
+/**************************************************************************************************
+*						removeItem
+*
+**************************************************************************************************/
+void List::removeItem(int remove_item)
+{
+	item_list.erase(item_list.begin()+remove_item);
 }
+ 
+/**************************************************************************************************
+*						displayList
+*
+**************************************************************************************************/
+void List::displayList()
+{
+	int length = item_list.size();	// variable for the end of the loop
+	int item_count = 0;				// variable to count how many items are in the shopping list
+
+
+	std::cout << "\nThis is what is contain in your grocery list.\n" << std::endl;
+	
+	// loop through the vector displaying all the information from the class
+	for (int i = 0; i < length; i++)
+	{
+		// display basic information about the car
+		std::cout << "Name: " << item_list[i].getName() << std::endl;
+		std::cout << "Unit: " << item_list[i].getUnit() << std::endl;
+		std::cout << "Quantity: " << item_list[i].getQuantity() << std::endl;
+		std::cout << "Price: $" << std::fixed << std::setprecision(2) << item_list[i].getPrice() << std::endl;
+		
+		item_count++;	// increase the number of items in your grocery list
+			
+		std::cout << endl;	
+	}
+
+	std::cout << "The total number of items in your grocery list: " << item_count << std::endl; 
+}
+	
+/**************************************************************************************************
+*						saveList
+*
+**************************************************************************************************/
