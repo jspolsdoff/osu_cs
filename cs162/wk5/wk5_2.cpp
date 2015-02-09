28ceba85249b1cd341f35adb7ee1fd5940857484
James Spolsdoff <jspolsdoff@gmail.com>
2015-02-08 16:24:13 -0800

    Second filter
    Sending file to flip to debug second filter.

---
+++ cs162/wk5/wk5_2.cpp
@@ -0,0 +1,90 @@
+/**************************************************************************************************
+*Author:                 James Spolsdoff
+*Date Created:           2/08/15
+*Last Modification Date: 2/08/15
+*Filename:               lab5_1.cpp
+*
+*Overview
+*This program will act as a filter and using inheritance and polymorphism
+***************************************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// define base class that will be used for all file filters
+class Filter
+{
+	private:
+	
+	private:
+	virtual void doFilter() = 0;	// pure virtual function used to make transformations
+};
+	
+// define derived class based on above base
+class basicFilter:public Filter
+{
+	private:
+	
+	public:
+	virtual void doFilter(int key);	// filter function will create an unchanged copy of the original file
+};
+	
+int main()
+{
+	int key = 0;
+	basicFilter filter1;
+	
+	std::cout << "Please enter an integer encryption key: ";
+	cin >> key;
+
+	filter1.doFilter(key);
+
+	return 0;
+}
+
+// derived class member function
+/**************************************************************************************************
+ *						doFilter
+ *
+ **************************************************************************************************/
+ void basicFilter::doFilter(int key)
+ {
+	std::ifstream inputFile;	// create the input file stream
+	std::ofstream outputFile;	// create the outputfile stream
+	std::string input_line;
+	char x;
+		
+	// open input file
+	inputFile.open("original.txt");
+	
+	// open output file
+	outputFile.open("filtered.txt");
+	
+	// test for file open errors
+	if (inputFile && outputFile)
+	{	
+		// use while loop to transfer information from input to output file
+		while (inputFile)
+		{
+			getline(inputFile, input_line);
+			const char *y = input_line.c_str();
+			
+			for(int i = 0;i < input_line.length(); i++)
+			{
+				x = (y[i] + key) % 26;
+				
+				outputFile << x;
+			}	
+		}
+		
+		// close both files
+		inputFile.close();
+		outputFile.close();
+	}	
+	
+	// else error opening file display message
+	else
+	{
+		std::cout << "Error opening the files." << std::endl;
+	}
+ }
