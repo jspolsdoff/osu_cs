0fdf843125f6951606e7dff4bb96397ed2716cce
James Spolsdoff <jspolsdoff@gmail.com>
2015-03-13 08:21:29 -0700

    Added timer to the main function
    Time to test in flip

--- cs162/wk10/lab10.cpp
+++ cs162/wk10/lab10.cpp
@@ -21,6 +21,11 @@
 #include <stdlib.h>
 #include "FibonacciR.h"
 #include "FibonacciNR.h"
+#include <chrono>
+#include <algorithm>
+
+using namespace std::chrono;
+
 using namespace std;
 
 void Usage()
@@ -46,15 +51,35 @@ int main(int argc, char** args)
 		
 		cout<<"Calling Recursive Fibonacci implementation"<<endl;
 		
+		// start timer
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
+		
 		FibonacciR fr(n);
+		
+		// stop timer and calculate result
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 		
 		fr.PrintFibonacci();
 		
+		cout << "The time to run function is : " << duration << endl;
+		
 		cout<<"Calling Non-Recursive Fibonacci implementation"<<endl;
+		
+		// start timer
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
 		
 		FibonacciNR fnr(n);
 		
+		// stop timer and calculate result
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+		
 		fnr.PrintFibonacci();
+		
+		cout << "The time to run function is : " << duration << endl;
 		
 		cout << "Done!!!!" << endl; 
 		
